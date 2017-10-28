b6c9eec053049d48b3f958d89f4eed7bf1c11973
Никоноров Илья Дмитриевич <nikonorov.i@ccam.cmc.unn.net>
2017-10-28 15:36:51 +0300

    Project

--- Stack/TStack.h
+++ Stack/TStack.h
@@ -28,5 +28,43 @@ public:
 			Mas[i] = ts.Mas[i];
 		}
 	}
+	TStack &operator=(const TStack &ts)
+	{
+		MaxSize = ts.Max.Size;
+		Size = ts.Size;
+		Mas = new T [MaxSize];
+		for (int i = 0; i<Size; i++)
+		{
+			Mas[i] = ts.Mas[i];
+		}
+		return *this;
+	}
+	int IsEmpty()
+	{
+		if (Size==0) return 1;
+		return 0;
+	}
+	int IsFull()
+	{
+		if (Size==MaxSize) return 1;
+		return 0;
+	}
+	void Push (const T & El)
+	{
+		if (IsFull()) throw MaxSize;
+		Mas[Size]=El;
+		Size++;
+	}
+	T Top() const 
+	{
+		if (IsEmpty()) throw -1;
+		return Mas[Size-1];
+	}
+	T Pop()
+	{
+		if (IsEmpty()) throw -1;
+		Size--;
+		return Mas[Size];
+	}
 
 };
